079b73e9fa087ec0fad4f37652934a7c28d6514c
Konrad Gräfe <kgraefe@ponyslaystation>
2010-05-27 14:34:39 +0200

    * updated buld scripts

--- build-debian-package.sh
+++ build-debian-package.sh
@@ -3,48 +3,68 @@
 PROJECT=pidgin-znchelper
 VERSION=$(cat VERSION)
 REPOSITORY=ppa:konradgraefe/pidgin-plugins
-DEB_REVISION=1
+DISTRIBUTIONS="karmic lucid maverick"
+
+if [ -f DEB_REVISION ]; then
+	DEB_REVISION=$(cat DEB_REVISION)
+else
+	DEB_REVISION=1
+fi
 
 src_dir=$(pwd)
 
 echo -n "make dist? (y/N) "
 read -n 1 in
 echo ""
-
 if [ "$in" == "y" ]; then
 	make dist
 fi
 
-rm -rf deb-pkg
-mkdir deb-pkg
-cd deb-pkg
-
-tar xzvf ../${PROJECT}-${VERSION}.tar.gz
-
-cd ${PROJECT}-${VERSION}
-
-cp -r ${src_dir}/debian .
-sed \
-	-e "s/@@VERSION@@/${VERSION}/" \
-	-e "s/@@DATE@@/$(date -R)/" \
-	-e "s/@@DEB_REVISION@@/${DEB_REVISION}/" \
-	debian/changelog.in >debian/changelog
-
-sed \
-	-e "s/@@DATE@@/$(date -R)/" \
-	debian/copyright.in >debian/copyright
-
-dpkg-buildpackage -S -rfakeroot
-
-cd ..
-lintian -i *.dsc
-
-echo ""
-echo -n "Upload ${PROJECT}_${VERSION}-${DEB_REVISION} to repository (y/N) "
-read -n 1 in
-echo ""
-
-if [ "$in" == "y" ]; then
-	dput ${REPOSITORY} ${PROJECT}_${VERSION}-${DEB_REVISION}_source.changes
+echo -n "Debian package revision? ($DEB_REVISION) "
+read in
+if [ "$in" != "" ]; then
+	DEB_REVISION=$in
 fi
 
+
+for dist in $DISTRIBUTIONS
+do
+	revision=$dist$DEB_REVISION
+
+	rm -rf deb-pkg
+	mkdir deb-pkg
+	cd deb-pkg
+
+	tar xzvf ../${PROJECT}-${VERSION}.tar.gz
+
+	cd ${PROJECT}-${VERSION}
+
+	cp -r ${src_dir}/debian .
+	sed \
+		-e "s/@@VERSION@@/${VERSION}/" \
+		-e "s/@@DATE@@/$(date -R)/" \
+		-e "s/@@DEB_REVISION@@/${revision}/" \
+		-e "s/@@DISTRIBUTION@@/${dist}/" \
+		debian/changelog.in >debian/changelog
+
+	sed \
+		-e "s/@@DATE@@/$(date -R)/" \
+		debian/copyright.in >debian/copyright
+
+	dpkg-buildpackage -S -rfakeroot
+
+	cd ..
+	lintian -i *.dsc
+
+	echo ""
+	echo -n "Upload ${PROJECT}_${VERSION}-${revision} to repository (y/N) "
+	read -n 1 in
+	echo ""
+
+	if [ "$in" == "y" ]; then
+		dput ${REPOSITORY} ${PROJECT}_${VERSION}-${revision}_source.changes
+		echo $DEB_REVISION >DEB_REVISION
+	fi
+
+	cd ${src_dir}
+done
