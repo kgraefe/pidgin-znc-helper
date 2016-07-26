6ea90023e4007414b8a333676d438c81ff7e222e
Konrad Gräfe <konradgraefe@aol.com>
2016-07-26 23:59:18 +0200

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 test -f VERSION || exit
-test -f ChangeLog|| exit
 test -f configure.ac.in || exit
+test -f Makefile.am.in || exit
 test -f COPYING || exit
 
-./po-update.sh || exit
-
 languages=""
-for f in po/*.po
-do test -f $f && languages="$languages $(basename $f .po)"
+for f in po/*.po; do
+	test -f $f && languages="$languages $(basename $f .po)"
 done
 
 headers=""
-for f in src/*.h
-do test -f $f && headers="$headers $f"
+for f in src/*.h; do
+	test -f $f && headers="$headers $f"
 done
 
-sed -e "s/@@VERSION@@/$(cat VERSION)/" -e "s/@@LANGUAGES@@/$(echo $languages)/" configure.ac.in >configure.ac || exit
-sed -e "s#@@HEADERFILES@@#$(echo $headers)#" Makefile.am.in >Makefile.am || exit
-aclocal || exit
-autoheader || exit
-libtoolize --copy || exit
-automake --add-missing --copy || exit
-autoconf || exit
-libtoolize --copy --install || exit
-intltoolize --copy --force  || exit
+sed \
+	-e "s/@@VERSION@@/$(cat VERSION)/" \
+	-e "s/@@LANGUAGES@@/$(echo $languages)/" \
+configure.ac.in >configure.ac || exit 1
 
+sed \
+	-e "s#@@HEADERFILES@@#$(echo $headers)#" \
+Makefile.am.in >Makefile.am || exit 1
+
+mkdir -p m4
+intltoolize --force --copy --automake || exit 1
+autoreconf --force --install --verbose || exit 1
