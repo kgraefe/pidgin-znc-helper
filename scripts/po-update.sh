df02f2a79298d5baa0147212374353c976222aed
Konrad Gräfe <konradgraefe@aol.com>
2016-08-01 13:03:48 +0200

    scripts/po-update.sh: Fix charset
    On some circumstances the intltool sets the charset the CHARSET, which
    breaks building the AppData manifest file.

--- scripts/po-update.sh
+++ scripts/po-update.sh
@@ -5,3 +5,7 @@ cd "$(dirname "$0")/../po"
 
 echo "Updating POT template..."
 intltool-update -pot -gettext-package=pidgin-znchelper || exit 1
+
+sed \
+	-i 's#^\"Content-Type: text/plain; charset=CHARSET\\n\"$#"Content-Type: text/plain; charset=UTF-8\\n"#g' \
+	pidgin-znchelper.pot
