58b6bfaaf8165e72e424dff05cfe0cd41126b298
Konrad Gräfe <konradgraefe@aol.com>
2018-02-05 14:30:48 +0100

    Update license headers
    This updates the license headers in the source files to the new year and
    a shorter form referencing the COPYING file.
    
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- src/pidgin_internals.h
+++ src/pidgin_internals.h
@@ -1,20 +1,7 @@
-/*
- * Pidgin ZNC Helper
- * Copyright (C) 2009-2016 Konrad Gräfe
+/* Copyright (C) 2009-2018 Konrad Gräfe <konradgraefe@aol.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1301, USA.
+ * This software may be modified and distributed under the terms
+ * of the GPLv2 license. See the COPYING file for details.
  */
 
 #ifndef PIDGIN_INTERNALS_H
