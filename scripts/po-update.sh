47cb125a851ca3d721fff31fa307b89bd58a40a2
Konrad Gräfe <konradgraefe@aol.com>
2018-02-07 16:02:41 +0100

    po-update.sh: Generate POTFILES.in
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- scripts/po-update.sh
+++ scripts/po-update.sh
@@ -3,11 +3,14 @@
 set -e
 set -o pipefail
 
+cd "$(dirname "$0")/.."
+
+ls -1 src/*.c > po/POTFILES.in
+ls -1 share/appdata/*.metainfo.xml.in >> po/POTFILES.in
+
 GETTEXT_PKG=$(awk -F= '/GETTEXT_PACKAGE=/ {print $2}' configure.ac)
 
-cd "$(dirname "$0")/../po"
-
-echo "Updating POT template..."
+cd po
 intltool-update -pot -gettext-package=$GETTEXT_PKG
 
 sed \
