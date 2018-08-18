28a932188c68e2631b71993d6fc12675069358ea
Konrad Gräfe <konradgraefe@aol.com>
2018-08-18 09:03:29 +0200

    Add self-message support
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- src/znchelper.c
+++ src/znchelper.c
@@ -23,6 +23,9 @@
 #define irc_next_field_or_return_false(s)  \
 		s = strchr(s, ' '); \
 		if(s) {s++;} else {return FALSE;}
+#define irc_next_field_or_return(s)  \
+		s = strchr(s, ' '); \
+		if(s) {s++;} else {return;}
 
 PurplePlugin *prpl_irc;
 PurplePluginProtocolInfo *irc_extra_info;
@@ -30,8 +33,12 @@ PurplePluginProtocolInfo *irc_extra_info;
 struct znc_conn {
 	gboolean server_time_enabled;
 	time_t server_time;
+
+	gboolean self_message_enabled;
+	gboolean self_message;
 };
 GHashTable *znc_conns;
+
 
 static void irc_send_raw(PurpleConnection *gc, const char *str) {
 	if(!irc_extra_info->send_raw) {
@@ -40,7 +47,6 @@ static void irc_send_raw(PurpleConnection *gc, const char *str) {
 	}
 	irc_extra_info->send_raw(gc, str, -1);
 }
-
 static void irc_sending_text_cb(PurpleConnection *gc, char **text, void *p) {
 	struct znc_conn *znc;
 
@@ -57,8 +63,10 @@ static void irc_sending_text_cb(PurpleConnection *gc, char **text, void *p) {
 		g_hash_table_insert(znc_conns, gc, znc);
 
 		irc_send_raw(gc, "CAP REQ :znc.in/server-time-iso\r\n");
+		irc_send_raw(gc, "CAP REQ :znc.in/self-message\r\n");
 	}
 }
+
 
 static void parse_server_time(
 	PurpleConnection *gc, struct znc_conn *znc, char **text
@@ -93,6 +101,64 @@ static void parse_server_time(
 		g_time_val_add(&t, offset * 60*60*1000*1000);
 	}
 	znc->server_time = (time_t)t.tv_sec;
+}
+static void parse_self_message(
+	PurpleConnection *gc, struct znc_conn *znc, char **text
+) {
+	/* Example:
+	 *     :ploppy!~kgraefe@p54BCACA3.dip0.t-ipconnect.de PRIVMSG kgraefe :self-message text
+	 */
+
+	char *p = *text;
+	char *from, *to, *msg, *end, *new;
+	const char *username;
+
+	if(*p != ':') {
+		return;
+	}
+	from = p + 1;
+
+	irc_next_field_or_return(p);
+	if(strncmp("PRIVMSG ", p, 8) != 0) {
+		return;
+	}
+
+	username = purple_connection_get_display_name(gc);
+
+	end = strchr(from, '!');
+	if(!end) {
+		end = p - 1;
+	}
+	if(
+		strlen(username) != (end - from) ||
+		strncmp(username, from, (end - from)) != 0
+	) {
+		return;
+	}
+
+	irc_next_field_or_return(p);
+	to = p;
+	if(*to == '#') {
+		return;
+	}
+
+	/* This is a self-message in a private chat. We need to swap the sender and
+	 * recipient in order to get the message through the IRC plugin. Luckily we
+	 * know that the new raw message will be shorter or equal to the original
+	 * message so we can just overwrite the buffer.
+	 */
+	irc_next_field_or_return(p);
+	msg = p;
+
+	new = g_strdup_printf(
+		":%.*s PRIVMSG %.*s %s"
+		, (msg - 1 - to), to
+		, (end - from), from
+		, msg
+	);
+	g_strlcpy(*text, new, strlen(*text));
+	g_free(new);
+	znc->self_message = TRUE;
 }
 static gboolean matches_cap_ack(const char *cap, const char *text) {
 	const char *p = text;
@@ -140,50 +206,94 @@ static void irc_receiving_text_cb(PurpleConnection *gc, char **text, void *p) {
 		if(matches_cap_ack(":znc.in/server-time-iso", *text)) {
 			znc->server_time_enabled = TRUE;
 		}
+		if(matches_cap_ack(":znc.in/self-message", *text)) {
+			znc->self_message_enabled = TRUE;
+		}
 	}
 
 	if(znc->server_time_enabled) {
 		parse_server_time(gc, znc, text);
 	}
+	if(znc->self_message_enabled) {
+		parse_self_message(gc, znc, text);
+	}
 }
 
-static void (*pidgin_conv_write_conv)(
-	PurpleConversation *conv, const char *name, const char *alias,
-	const char *message, PurpleMessageFlags flags, time_t mtime
-);
-static void znc_write_conv(
-	PurpleConversation *conv, const char *name, const char *alias,
-	const char *message, PurpleMessageFlags flags, time_t mtime
-) {
+
+static struct znc_conn *conversation_get_znc(PurpleConversation *conv) {
 	PurpleConnection *gc;
 	PurpleAccount *account;
-	struct znc_conn *znc;
 
 	account = purple_conversation_get_account(conv);
 	if(!account) {
-		goto exit;
+		return NULL;
 	}
 	gc = purple_account_get_connection(account);
 	if(!gc) {
-		goto exit;
+		return NULL;
 	}
+	return g_hash_table_lookup(znc_conns, gc);
+}
 
-	znc = g_hash_table_lookup(znc_conns, gc);
+
+static void (*pidgin_write_chat)(
+	PurpleConversation *conv, const char *who, const char *message,
+	PurpleMessageFlags flags, time_t mtime
+);
+static void znc_write_chat(
+	PurpleConversation *conv, const char *who, const char *message,
+	PurpleMessageFlags flags, time_t mtime
+) {
+	struct znc_conn *znc;
+
+	znc = conversation_get_znc(conv);
 	if(!znc) {
 		goto exit;
 	}
+
 	if(znc->server_time) {
 		mtime = znc->server_time;
 		znc->server_time = 0;
 	}
 
 exit:
-	pidgin_conv_write_conv(conv, name, alias, message, flags, mtime);
+	pidgin_write_chat(conv, who, message, flags, mtime);
+}
+
+
+static void (*pidgin_write_im)(
+	PurpleConversation *conv, const char *who,
+   const char *message, PurpleMessageFlags flags, time_t mtime
+);
+static void znc_write_im(
+	PurpleConversation *conv, const char *who,
+   const char *message, PurpleMessageFlags flags, time_t mtime
+) {
+	struct znc_conn *znc;
+
+	znc = conversation_get_znc(conv);
+	if(!znc) {
+		goto exit;
+	}
+
+	if(znc->server_time) {
+		mtime = znc->server_time;
+		znc->server_time = 0;
+	}
+	if(znc->self_message) {
+		flags &= ~PURPLE_MESSAGE_RECV;
+		flags |= PURPLE_MESSAGE_SEND;
+
+		znc->self_message = FALSE;
+	}
+
+exit:
+	pidgin_write_im(conv, who, message, flags, mtime);
 }
 
 
 static gboolean plugin_load(PurplePlugin *plugin) {
-	PurpleConversationUiOps *conversation_ui_ops;
+	PurpleConversationUiOps *ops;
 
 	prpl_irc = purple_find_prpl("prpl-irc");
 	if(!prpl_irc || !prpl_irc->info || !prpl_irc->info->extra_info) {
@@ -203,19 +313,38 @@ static gboolean plugin_load(PurplePlugin *plugin) {
 		PURPLE_CALLBACK(irc_sending_text_cb), NULL
 	);
 
-	/* Let's hook into conversation between Pidgin and libpurple */
-	conversation_ui_ops = pidgin_conversations_get_conv_ui_ops();
-	pidgin_conv_write_conv = conversation_ui_ops->write_conv;
-	conversation_ui_ops->write_conv = znc_write_conv;
-	
+	/* Hook into conversation between Pidgin and libpurple */
+	ops = pidgin_conversations_get_conv_ui_ops();
+
+	pidgin_write_chat = ops->write_chat;
+	if(!pidgin_write_chat) {
+		pidgin_write_chat = purple_conversation_write;
+	}
+	ops->write_chat = znc_write_chat;
+
+	pidgin_write_im = ops->write_im;
+	if(!pidgin_write_im) {
+		pidgin_write_im = purple_conversation_write;
+	}
+	ops->write_im = znc_write_im;
+
 	return TRUE;
 }
 static gboolean plugin_unload(PurplePlugin *plugin) {
-	PurpleConversationUiOps *conversation_ui_ops;
+	PurpleConversationUiOps *ops;
 
-	conversation_ui_ops = pidgin_conversations_get_conv_ui_ops();
-	if(conversation_ui_ops) {
-		conversation_ui_ops->write_conv = pidgin_conv_write_conv;;
+	ops = pidgin_conversations_get_conv_ui_ops();
+	if(ops) {
+		if(pidgin_write_chat == purple_conversation_write) {
+			ops->write_chat = NULL;
+		} else {
+			ops->write_chat = pidgin_write_chat;
+		}
+		if(pidgin_write_im == purple_conversation_write) {
+			ops->write_im = NULL;
+		} else {
+			ops->write_im = pidgin_write_im;
+		}
 	}
 
 	purple_signal_disconnect(
