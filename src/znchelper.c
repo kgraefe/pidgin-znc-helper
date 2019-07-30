e99e77d18f4c6e300fac05be31ae244871a18603
Patrick Thomas <pwthomas95@zoho.com>
2019-07-30 17:59:29 -0500

    Added code safety & separated declaration of gc

--- src/znchelper.c
+++ src/znchelper.c
@@ -98,12 +98,13 @@ static void znc_write_chat(
 	PurpleConversation *conv, const char *who, const char *message,
 	PurpleMessageFlags flags, time_t mtime
 ) {
-	PurpleConnection *gc = purple_conversation_get_gc(conv);
+	PurpleConnection *gc;
 	PurpleConvChat *chat;
 	struct znc_conn *znc;
 	GList *parted, *l;
 	gint state;
-
+	
+	gc = purple_conversation_get_gc(conv);
 	znc = conversation_get_znc(conv);
 	if(!znc) {
 		goto exit;
@@ -123,7 +124,7 @@ static void znc_write_chat(
 		state = GPOINTER_TO_INT(purple_conversation_get_data(conv, "znc-state"));
 		switch(state) {
 		case ZNC_CONV_STATE_START:
-			if(!purple_account_get_bool(gc->account, "hide_znc_playback_message", FALSE)) {
+			if(!gc || !purple_account_get_bool(gc->account, "hide_znc_playback_message", FALSE)) {
 				ui_write_chat(
 					conv, "***", _("Buffer Playback..."),
 					PURPLE_MESSAGE_NO_LOG | PURPLE_MESSAGE_SYSTEM, time(NULL)
